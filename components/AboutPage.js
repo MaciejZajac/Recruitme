009dd7fd6f3d7064a439f9dee66c4f749f2e3a3f
mzajac <mzajac@corelogic.pl>
2020-07-04 19:58:11 +0200

    dodanie sekcji z ofertami

--- components/AboutPage.js
+++ components/AboutPage.js
@@ -5,9 +5,7 @@ const AboutPage = () => {
         <>
             <div className='home home--30'>
                 <div className='about__container'>
-                    <h2 className='about__title'>
-                        Pierwsza agencja rekrutacyjna IT dedykowana start-upom i firmom produktowym
-                    </h2>
+                    <h2 className='about__title'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h2>
                 </div>
             </div>
             <section className='section'>
@@ -16,13 +14,10 @@ const AboutPage = () => {
                         <div className='container'>
                             <h1 className='title'>Kim jesteśmy ?</h1>
                             <h2 className='subtitle'>
-                                Działamy w branży IT od 2005 roku. Współpracujemy z globalnymi korporacjami, a także
-                                innymi firmami, głównie z Europy. Specjalizujemy się w tworzeniu oprogramowania, ale
-                                mamy również duże doświadczenie w outsourcingu specjalistów i zespołów IT, a także w
-                                modelu Build-Operate-Transfer.
-                                <br />
-                                Jesteśmy aktywnym członkiem rynku IT w Krakowie i Polsce, uczestniczymy w wydarzeniach
-                                branżowych jak i sami je organizujemy.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel pretium turpis, vitae
+                                blandit ex. Quisque sodales velit nec enim consequat consectetur. Cras lacinia rhoncus
+                                purus, at lobortis velit ornare luctus. Duis semper odio neque, a vulputate velit
+                                commodo quis. Morbi auctor pharetra risus, sodales pulvinar metus auctor ac.
                             </h2>
                         </div>
                     </div>
--- components/HomePage.js
+++ components/HomePage.js
@@ -59,13 +59,45 @@ function HomePage() {
                     </div>
                 </div>
             </div>
-            <section className='section'>
-                <div className='container'>
-                    <h1 className='title'>Zobacz jak możemy wesprzeć twój biznes</h1>
-                    <h2 className='subtitle'>
-                        Rozumiemy otoczenie biznesowe w którym działamy, dlatego proponujemy i wdrażamy rozwiązania IT
-                        najlepsze dla Twoich potrzeb biznesowych.
-                    </h2>
+            <section class='hero'>
+                <div class='hero-body'>
+                    <div class='container'>
+                        <h1 class='title'>Zobacz jak możemy wesprzeć twój biznes</h1>
+                        <h2 class='subtitle'>
+                            Rozumiemy otoczenie biznesowe w którym działamy, dlatego proponujemy i wdrażamy rozwiązania
+                            IT najlepsze dla Twoich potrzeb biznesowych.
+                        </h2>
+                    </div>
+                </div>
+            </section>
+            <section class='section'>
+                <div className='columns pb-6 '>
+                    <div className='column '>
+                        <div className='columns is-multiline'>
+                            {offerArr.map((item) => {
+                                let wynagrodzenie = '';
+                                if (item.salaryFrom) {
+                                    wynagrodzenie += 'Od ' + item.salaryFrom + ' zł ';
+                                }
+                                if (item.salaryTo) {
+                                    wynagrodzenie += 'do ' + item.salaryTo + ' zł';
+                                }
+                                return (
+                                    <div className='column is-one-third-desktop' key={item.id}>
+                                        <div className='offer'>
+                                            Pozycja: {item.position}
+                                            <br />
+                                            Wynagrodzenie: {wynagrodzenie}
+                                            <br />
+                                            Doświadczenie: {item.experience} rok
+                                            <br />
+                                            Lokalizacja: {item.city}
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
@@ -73,3 +105,56 @@ function HomePage() {
 }
 
 export default HomePage;
+
+const offerArr = [
+    {
+        position: 'Front-end developer',
+        mainTech: ['React', 'JavaScript', 'HTML', 'CSS'],
+        salaryFrom: 5000,
+        salaryTo: 10000,
+        city: 'Kraków',
+        experience: 2,
+        id: 1,
+    },
+    {
+        position: 'Back-end developer',
+        mainTech: ['Java', 'Spring', 'Hibernate'],
+        salaryFrom: 6500,
+        salaryTo: 11000,
+        city: 'Kraków',
+        experience: 2.5,
+        id: 2,
+    },
+    {
+        position: 'DevOps Engineer',
+        salaryFrom: 8000,
+        salaryTo: 14000,
+        city: 'Kraków',
+        experience: 3,
+        id: 3,
+    },
+    {
+        position: 'UX/UI designer',
+        salaryFrom: 4500,
+        salaryTo: 7800,
+        city: 'Kraków',
+        experience: 1,
+        id: 4,
+    },
+    {
+        position: 'Project Manager',
+        salaryFrom: 8000,
+        salaryTo: 15000,
+        city: 'Kraków',
+        experience: 3,
+        id: 5,
+    },
+    {
+        position: 'QA Tester',
+        salaryFrom: 6000,
+        salaryTo: 90000,
+        city: 'Kraków',
+        experience: 1.5,
+        id: 6,
+    },
+];
